c47830d3c75da6c0a10a4cbcb7fd9ad129fa31dd
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-01-15 17:09:28 +0100

    fix: search bar keeps the search term after search

--- src/components/Header/components/SearchBar.tsx
+++ src/components/Header/components/SearchBar.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image";
 import styles from "../styles.module.css";
+import { useSearchParams } from "next/navigation";
 
 export default function SearchBar() {
+  const searchParams = useSearchParams();
   return (
     <form className={styles.searchBar} action="/products">
       <Image src="icons/search.svg" alt="Search icon" width={24} height={24} />
-      <input type="text" placeholder="Search" name="q" />
+      <input
+        type="text"
+        placeholder="Search"
+        name="q"
+        defaultValue={searchParams.get("q") || ""}
+      />
     </form>
   );
 }
