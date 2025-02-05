87d0c0a1c94aba39ccbe4e2be1ec3daa70287025
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-02-05 21:51:28 +0100

    fix: filter indicator should only react to price or categories

--- src/app/products/components/FiltersSection/FiltersSection.tsx
+++ src/app/products/components/FiltersSection/FiltersSection.tsx
@@ -4,12 +4,13 @@ import Link from "next/link";
 import styles from "./styles.module.css";
 import CategoriesFilter from "../CategoriesFilter";
 import PriceFilter from "../PriceFilter/PriceFilter";
-import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
 export default function FiltersSection() {
   const searchParams = useSearchParams();
-  const searchParamsLength = Array.from(searchParams.entries()).length;
+  const filtersLength = Array.from(searchParams.entries()).filter((param) => {
+    return param[0] === "category" || param[0] === "price";
+  }).length;
   return (
     <aside>
       <div>
@@ -19,21 +20,17 @@ export default function FiltersSection() {
               <h3>Filter</h3>
               <span
                 className={styles.filterCount}
-                data-visible={searchParamsLength > 0 ? true : false}
+                data-visible={filtersLength > 0 ? true : false}
               >
-                {searchParamsLength}
+                {filtersLength}
               </span>
             </div>
             <Link href="/products" className={styles.productsFiltersClearAll}>
               Reset
             </Link>
           </div>
-          <Suspense fallback={<div>Loading price filter...</div>}>
-            <PriceFilter />
-          </Suspense>
-          <Suspense fallback={<div>Loading categories filter...</div>}>
-            <CategoriesFilter />
-          </Suspense>
+          <PriceFilter />
+          <CategoriesFilter />
         </div>
       </div>
     </aside>
