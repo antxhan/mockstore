ec9997f4d975fcd440ffdb766b91982e56a6d992
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-02-05 16:58:24 +0100

    refactor: put filter icon in own icon component

--- src/app/products/components/SortBy/index.tsx
+++ src/app/products/components/SortBy/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./styles.module.css";
 import useFilter from "../../hooks/useFilter";
+import FilterIcon from "@/icons/FilterIcon";
 
 export default function SortBy() {
   const { searchParams, applyFilter } = useFilter();
@@ -10,20 +11,7 @@ export default function SortBy() {
   return (
     <div className={styles.sort}>
       <div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M4 4h16v2.172a2 2 0 0 1 -.586 1.414l-4.414 4.414v7l-6 2v-8.5l-4.48 -4.928a2 2 0 0 1 -.52 -1.345v-2.227z" />
-        </svg>
+        <FilterIcon />
         <span>Sort by:</span>
       </div>
       <select
