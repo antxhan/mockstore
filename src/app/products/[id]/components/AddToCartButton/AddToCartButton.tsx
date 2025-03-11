712bdf1da4a54a96e80f1489f7ce496d8fb24029
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-03-11 14:56:01 +0100

    feat: added to cart change button on click

--- src/app/products/[id]/components/AddToCartButton/AddToCartButton.tsx
+++ src/app/products/[id]/components/AddToCartButton/AddToCartButton.tsx
@@ -6,6 +6,10 @@ import styles from "./styles.module.css";
 import { useDBContext } from "@/contexts/db";
 import { Product } from "@/lib/types";
 import { db } from "@/utils/db";
+import { sleep } from "@/utils/utils";
+import { useRef, useState } from "react";
+import HeartOutlineIcon from "@/icons/HeartOutlineIcon";
+import CircleCheckIcon from "@/icons/CircleCheckIcon";
 
 export default function AddToCartButton({
   productId,
@@ -15,16 +19,38 @@ export default function AddToCartButton({
   quantity: number;
 }) {
   const { setCart } = useDBContext();
-  const handleAddToCart = () => {
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+  const [buttonIcon, setButtonIcon] = useState<React.ReactNode>(<CartIcon />);
+  const [buttonText, setButtonText] = useState("Add to Cart");
+
+  const handleAddToCart = async () => {
+    // e.stopPropagation();
+    // e.preventDefault();
+    // const button = e.target as HTMLButtonElement;
     db.cart.add(productId, quantity);
     setCart(db.cart.get());
+
+    // setTimeout(() => {
+    //   e.currentTarget.classList.add(styles.add);
+    // }, 1000);
+
+    if (buttonRef.current) {
+      buttonRef.current.classList.add(styles.add);
+      setButtonIcon(<CircleCheckIcon />);
+      setButtonText("Added to Cart");
+      await sleep(1000);
+      buttonRef.current.classList.remove(styles.add);
+      setButtonIcon(<CartIcon />);
+      setButtonText("Add to Cart");
+    }
   };
   return (
     <MainButton
-      icon={<CartIcon />}
-      title="Add to Cart"
+      icon={buttonIcon}
+      title={buttonText}
       className={styles.addToCartButton}
       onClick={handleAddToCart}
+      ref={buttonRef}
     />
   );
 }
